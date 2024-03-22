c48993932d4a57e604c5a831d5dd822944873f89
daaaaahling <una_cruz@yahoo.com>
2024-03-22 15:38:09 +0000

    betty

--- search_and_execute.c
+++ search_and_execute.c
@@ -16,35 +16,35 @@
  */
 int search_and_execute(char *path_env, char **dirs, char **args)
 {
-    int idx = 0;
-    char *full_path = NULL;
-    struct stat file_stat;
-
-    /* Iterate through each directory in the path array */
-    while (dirs[idx] != NULL)
-    {
-        /* Construct full path by concatenating directory and command */
-        full_path = malloc(strlen(args[0]) + strlen(dirs[idx]) + 2);
-        if (full_path == NULL)
-            exit(EXIT_FAILURE);
-
-        strcpy(full_path, dirs[idx]);
-        strcat(full_path, "/");
-        strcat(full_path, args[0]);
-
-        /* Check if constructed path corresponds to an existing file */
-        if (stat(full_path, &file_stat) == 0)
-        {
-            /* If file exists, execute the command */
-            run_child(path_env, full_path, args);
-            free(full_path);
-            return (0);
-        }
-        /* Free dynamically allocated memory for constructed path */
-        free(full_path);
-        /* Move to the next directory in the path array */
-        idx++;
-    }
-    /* If command is not found in any directory, return exit status 127 */
-    return (127); /* command not found */
+	int idx = 0;
+	char *full_path = NULL;
+	struct stat file_stat;
+	
+	/* Iterate through each directory in the path array */
+	while (dirs[idx] != NULL)
+	{
+		/* Construct full path by concatenating directory and command */
+		full_path = malloc(strlen(args[0]) + strlen(dirs[idx]) + 2);
+		if (full_path == NULL)
+			exit(EXIT_FAILURE);
+		
+		strcpy(full_path, dirs[idx]);
+		strcat(full_path, "/");
+		strcat(full_path, args[0]);
+		
+		/* Check if constructed path corresponds to an existing file */
+		if (stat(full_path, &file_stat) == 0)
+		{
+			/* If file exists, execute the command */
+			run_child(path_env, full_path, args);
+			free(full_path);
+			return (0);
+		}
+		/* Free dynamically allocated memory for constructed path */
+		free(full_path);
+		/* Move to the next directory in the path array */
+		idx++;
+	}
+	/* If command is not found in any directory, return exit status 127 */
+	return (127); /* command not found */
 }
