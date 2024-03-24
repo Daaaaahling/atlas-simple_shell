748073383e07b8cbaf0fa8d3be2407e625931dd4
daaaaahling <una_cruz@yahoo.com>
2024-03-24 22:19:35 +0000

    troubleshooting why we lost point with env

--- handle_command.c
+++ handle_command.c
@@ -10,18 +10,27 @@
 int handle_command(char **env, char **path, char **paths, char **tokens)
 {
 	int exit_status = 0;
+	int i;
 
 	if (!tokens[0])
+	{
+		/* Empty input, return 0 as exit status */
 		return (0);
 
 	if (strcmp(tokens[0], "exit") == 0)
-	{
+	
 		free(*path);
 		exit(EXIT_SUCCESS);
 	}
 	if (strcmp(tokens[0], "env") == 0)
 	{
-		display_env(env);
+		/* Display environment variables */
+		for (i = 0; env[i] != NULL; i++)
+		{
+			write(STDOUT_FILENO, env[i], strlen(env[i]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		/* Return 0 as exit status */
 		return (0);
 	}
 	if (access(tokens[0], X_OK) == 0)
